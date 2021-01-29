a63f573cbc97f6954a00bec28ff9edc90fa229af
Kyle <kylehunt.animator@gmail.com>
2021-01-29 14:45:05 -0800

    Default address input country to Canada

--- app/js/controllers/addressInputCtrl.js
+++ app/js/controllers/addressInputCtrl.js
@@ -42,12 +42,12 @@ function ($scope, $rootScope, $location, User, Address, Resources) {
     var streetName = null;
     var tempShipping = null;
     var tempBilling = null;
-
+	$scope.address = {'Country':"CA"};
     $scope.$watch('addressobj', function(newval, oldval) {
         if (newval != oldval){
             $scope.address.IsShipping   ? (tempShipping = true) : '';
             $scope.address.IsBilling    ? (tempBilling = true)  : '';
-            $scope.address = {'Country':"US"};
+            $scope.address = {'Country':"CA"};
             tempShipping    ? $scope.address.IsShipping = tempShipping  : '';
             tempBilling     ? $scope.address.IsBilling  = tempBilling   : '';
             streetComplete  = null;
